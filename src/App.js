5e80460fd4acf6905bf7a6239b7f38944dd7f4ee
JudyPrograms <judithkats@gmail.com>
2021-11-01 16:06:04 +0100

    setup-play-page-flexbox-card

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import {Switch, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Play from "./pages/Play";
+import Account from "./pages/Account";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+
 
 
 function App() {
   return (
     <div>
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-
-        <Play/>
-
+        <Switch>
+            <Route exact path="/">
+                <Play/>
+            </Route>
+            <Route exact path="/play">
+                <Play/>
+            </Route>
+            <Route exact path="/dashboard">
+                <Dashboard/>
+            </Route>
+            <Route exact path="/settings">
+                <Settings/>
+            </Route>
+            <Route exact path="/account">
+                <Account/>
+            </Route>
+        </Switch>
     </div>
   );
 }
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import {useHistory, Link} from 'react-router-dom';
 import avatar from "../assets/blanco-avatar.png";
 import logo from "../assets/app-logo.png";
+import target from "../assets/target.png";
+import mountain from "../assets/mountain.png";
+import arch from "../assets/arch.png";
+
+
+const subtasks = [
+    {
+        "task": "Paperwork",
+        "slices": ["pay bills", "slice2", "slice3"]
+    },
+    {
+        "task": "Declutter",
+        "slices": ["wash clothes", "clean dishes", "tidy closet", "tidy room"]
+    },
+    {
+        "task": "Socialize",
+        "slices": ["pay bills", "slice2", "slice3"]
+    },
+    {
+        "task": "Selfcare",
+        "slices": ["pay bills", "slice2", "slice3"]
+    },
+]
 
 
 function Play() {
@@ -19,12 +43,45 @@ function Play() {
                 <img src={logo} alt="logo" className="header__app-logo"/>
             </header>
 
-            <div className="play-card">
-                <div className="title-bar">
-                    <img src="" alt="" className="title-bar__icon"/>
-                    <h1 className="title-bar__title">Pick a Challenge</h1>
+            <nav className="navbar">
+                <Link to="/play" className="navbar__link">PLAY</Link>
+                <Link to="/dashboard" className="navbar__link">DASHBOARD</Link>
+                <Link to="/settings" className="navbar__link">SETTINGS</Link>
+                <Link to="/account" className="navbar__link">ACCOUNT</Link>
+            </nav>
+
+            <div className="play-container">
+
+                <div className="play-card">
+                    <div className="title-bar">
+                        <img src={target} alt="target" className="title-bar__title-icon"/>
+                        <h1 className="title-bar__title">Pick a Challenge</h1>
+                    </div>
+                    <div className="card-content">
+                        <ul>
+                            {subtasks.map((item) => {
+                                return (
+                                <li>
+                                    <img src={arch} alt="arch" className="card-content__link-img"/>
+                                    <h2><a href="">{item.task}</a></h2>
+                                </li>)
+                            })}
+                        </ul>
+                    </div>
+                    <img src={mountain} alt="mountain" className="card-image"/>
                 </div>
+
+                <div className="button-box">
+                    <button type="button">
+                        BACK
+                    </button>
+                    <button type="button">
+                        NEXT
+                    </button>
+                </div>
+
             </div>
+
 
         </div>
     );
